717ade9e538eeebc5b0a62ce825f41fb5ab3986a
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2019-01-05 13:23:30 +0300

    Init React App and add Redux

---
+++ src/component/UserList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+	render() {
+		return (
+			<div>
+				<h2>User List</h2>
+			</div>
+		)
+	}
+}
+
+export default UserList;
